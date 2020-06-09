171254053a3a7810fa7739a099a37240e10307e2
nssina <rabie_sina@yahoo.com>
2020-06-09 08:16:55 -0400

    Complete expense tracker

--- arrays/expense-tracker.js
+++ arrays/expense-tracker.js
@@ -1,23 +1,41 @@
 const account = {
     name: "Sina Rabiei",
     expenses: [],
+    income: [],
     addExpense: function (description, amount) {
         this.expenses.push({
             description: description,
             amount: amount
         })
     },
+    addIncome: function (description, amount) {
+        this.income.push({
+            description: description,
+            amount: amount
+        })
+    },
     getAccountSummary: function () {
         let totalExpenses = 0
+        let totalIncome = 0
+        let accountBalance = 0
 
         this.expenses.forEach(function (expense) {
             totalExpenses = totalExpenses + expense.amount
         })
 
-        return `${this.name} has $${totalExpenses} in expenses.`
+        this.income.forEach(function (income) {
+            totalIncome = totalIncome + income.amount
+        })
+
+        accountBalance = totalIncome - totalExpenses
+
+        return `${this.name} has a balance of $${accountBalance}. $${totalIncome} in income. $${totalExpenses} in expenses.`
     }
 }
+
+
  
 account.addExpense('Rent', 950)
 account.addExpense('Coffee', 2)
+account.addIncome('Job', 1000)
 console.log(account.getAccountSummary())
