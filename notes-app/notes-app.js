21e12fd40dc069d9e7ef983f3582cbe82b9a54ee
nssina <rabie_sina@yahoo.com>
2020-06-11 08:08:39 -0400

    Added query for one or all elements

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,1 +1,11 @@
-console.log("This is from a different file")
+// DOM - Document Object Model
+
+// Query and remove
+// const p = document.querySelector("p")
+// p.remove()
+
+// Query all and remove
+const ps = document.querySelectorAll("p")
+ps.forEach(function (p) {
+    p.remove()
+})
