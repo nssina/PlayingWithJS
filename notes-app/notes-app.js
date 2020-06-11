bd863bcec9c9f995ace49a21aebb4ac84bfe2b33
nssina <rabie_sina@yahoo.com>
2020-06-11 09:43:28 -0400

    Added incompleted todos counter and showing in the web

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -7,6 +7,11 @@
 // Query all and remove
 const ps = document.querySelectorAll("p")
 ps.forEach(function (p) {
-    p.textContent = "*******"
+    // p.textContent = "*******"
     // console.log(p.textContent)
 })
+
+// Add a new element
+const newP = document.createElement('p')
+newP.textContent = "This a new element from JavaScript"
+document.querySelector("body").appendChild(newP)
---
+++ todo-app/todo-app.js
@@ -0,0 +1,24 @@
+const todos = [{
+    text: "Order cat food",
+    completed: true
+}, {
+   text: "Clean kitchen",
+   completed: false
+}, {
+   text: "Buy food",
+   completed: true
+}, {
+   text: "Do work",
+   completed: false
+}, {
+   text: "Exercise",
+   completed: true
+}]
+
+const incompleteTodos = todos.filter(function (todo) {
+   return !todo.completed
+})
+
+const summary = document.createElement("h2")
+summary.textContent = `You have ${incompleteTodos.length} todos left`
+document.querySelector("body").appendChild(summary)
