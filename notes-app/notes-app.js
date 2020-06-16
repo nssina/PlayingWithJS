62ea31235c836c94ae7acb7b1851f6cff3f496f8
nssina <rabie_sina@yahoo.com>
2020-06-16 14:08:38 -0400

    Added form for notes app

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -34,13 +34,13 @@ document.querySelector('#create-note').addEventListener('click', function (e) {
     e.target.textContent = "The button was clicked"
 })
 
-document.querySelector('#remove-all').addEventListener('click', function (e) {
-    document.querySelectorAll('.note').forEach(function (note) {
-        note.remove()
-    })
-})
-
 document.querySelector('#search-text').addEventListener('input', function(e) {
     filters.searchText = e.target.value
     renderNotes(notes, filters)
+})
+
+document.querySelector('#name-form').addEventListener('submit', function (e) {
+    e.preventDefault()
+    console.log(e.target.elements.firstName.value)
+    e.target.elements.firstName.value = ''
 })
