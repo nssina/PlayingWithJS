bacf335954f91a17bd918083637c83fe21cc1b6e
nssina <rabie_sina@yahoo.com>
2020-06-16 13:47:08 -0400

    Search and filter for todos

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -15,19 +15,34 @@ const todos = [{
    completed: true
 }]
 
-const incompleteTodos = todos.filter(function (todo) {
-   return !todo.completed
-})
+const filters = {
+   searchText: ''
+}
 
-const summary = document.createElement("h2")
-summary.textContent = `You have ${incompleteTodos.length} todos left`
-document.querySelector("body").appendChild(summary)
+const renderTodos = function (todos, filters) {
 
-todos.forEach(function (todo) {
-   const p = document.createElement("p")
-   p.textContent = todo.text
-   document.querySelector("body").appendChild(p)
-})
+   const filteredTodos = todos.filter(function (todo) {
+      return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+   })
+
+   const incompleteTodos = filteredTodos.filter(function (todo) {
+      return !todo.completed
+   })
+
+   document.querySelector("#todos").innerHTML = ""
+   
+   const summary = document.createElement("h2")
+   summary.textContent = `You have ${incompleteTodos.length} todos left`
+   document.querySelector("#todos").appendChild(summary)
+   
+   filteredTodos.forEach(function (todo) {
+      const p = document.createElement("p")
+      p.textContent = todo.text
+      document.querySelector("#todos").appendChild(p)
+   })
+}
+
+renderTodos(todos, filters)
 
 // Listen for new todo creation
 
@@ -37,4 +52,9 @@ document.querySelector('#add-todo').addEventListener('click', function(e) {
 
 document.querySelector("#new-todo-text").addEventListener("input", function(e) {
    console.log(e.target.value)
+})
+
+document.querySelector("#search-text").addEventListener("input", function (e) {
+   filters.searchText = e.target.value
+   renderTodos(todos, filters)
 })
