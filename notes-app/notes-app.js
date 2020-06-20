028246b9a2a8cffc4fefda8d2b19f1d68f589073
nssina <rabie_sina@yahoo.com>
2020-06-20 10:49:57 -0400

    Added checkbox for notes app

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -39,8 +39,6 @@ document.querySelector('#search-text').addEventListener('input', function(e) {
     renderNotes(notes, filters)
 })
 
-document.querySelector('#name-form').addEventListener('submit', function (e) {
-    e.preventDefault()
-    console.log(e.target.elements.firstName.value)
-    e.target.elements.firstName.value = ''
+document.querySelector('#for-fun').addEventListener('change', function (e) {
+    console.log(e.target.checked)
 })
